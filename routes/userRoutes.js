6b8ce2a3cd49adf2ff200df42385ec689de6dbcc
Johnny Chen <Johnnychen384@gmail.com>
2023-03-09 16:46:13 -0500

    finished put route for userRoutes file

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -25,14 +25,11 @@ router.get("/:id/shows", async (req, res) => {
 
     try{
 
-        const userWithShows = await User.findByPk(req.params.id, {
-            include: [
-                {model: Show}
-            ]
-        })
+        const userWithShows = await User.findByPk(req.params.id)
 
         if(Object.keys(userWithShows).includes("dataValues")){
-            res.status(200).json(userWithShows)
+            const shows = await userWithShows.getShows()
+            res.status(200).json(shows)
         } else {
             res.status(200).send("No User Found.")
         }
@@ -66,7 +63,29 @@ router.get("/:id", async (req, res) => {
 })
 
 
-router.put()
+router.put("/:id", async (req, res) => {
+    try{
+        const currentUser = await User.findByPk(req.params.id);
+        const showArr = await Show.findAll();
+        const watchedShow = req.body.title
+
+        for(let show of showArr){
+            const showValues = Object.values(show)
+
+            if(showValues[0].title === watchedShow) {
+                currentUser.addShow(show.dataValues.id)
+            }
+        }
+
+        const watchedShowsArr = await currentUser.getShows()
+
+        res.status(200).json(watchedShowsArr)
+
+    } catch(err) {
+        console.error(err)
+        res.status(404).send("There is a problem with finding this User.")
+    }
+})
 
 
 
